3b2d40859b4289a6634af808dea80e6b52e365eb
shoman <shoma216@gmail.com>
2019-06-09 01:03:26 +0900

    fix ui in challenge list page

--- src/app/challenge-list/challenge-list.component.ts
+++ src/app/challenge-list/challenge-list.component.ts
@@ -14,16 +14,19 @@ export class ChallengeListComponent implements OnInit {
 
   challenges = [
       {
+          challenger: '保険太郎',
           key: 'スマートフォン',
           back: '200',
           deadline: '2019/06/08'
       },
       {
+          challenger: '保険太郎',
           key: 'PC',
           back: '500',
           deadline: '2019/06/08'
       },
       {
+          challenger: '保険太郎',
           key: 'スマートフォン',
           back: '500',
           deadline: '2019/06/08'
