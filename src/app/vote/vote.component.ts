c4c5cd4b6d6a95d5c68bb2c764aa76e88d83cb12
shoman <shoma216@gmail.com>
2019-06-08 16:45:39 +0900

    add vote page

---
+++ src/app/vote/vote.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vote',
+  templateUrl: './vote.component.html',
+  styleUrls: ['./vote.component.css']
+})
+export class VoteComponent implements OnInit {
+
+  constructor() { }
+  ngOnInit() {
+  }
+
+  applicantName = '保険太郎'
+  insuranceType = 'スマホ'
+  deadline = '2019/06/08'
+
+  depositFee = 0.01
+}
