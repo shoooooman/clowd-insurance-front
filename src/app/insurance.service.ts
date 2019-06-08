52623708375d47a050da280b9da1654975a6d758
admin <k.kubo.private.mobile@gmail.com>
2019-06-08 22:20:09 +0900

    'insurance'

---
+++ src/app/insurance.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InsuranceService {
+
+  collection() {
+    return this.angularFirestore.collection('insurances');
+  }
+
+  get() {
+    return this.collection().get();
+  }
+
+  constructor(
+    private angularFirestore: AngularFirestore
+  ) {
+  }
+}
